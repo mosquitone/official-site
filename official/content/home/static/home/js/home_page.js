12040cd1f68da87555890dbfe3c604b73623cf8b
Yuusuke Yamashita <yamashita@yyamashita.com>
2019-09-01 21:41:18 +0900

    add transitional link button to homepage

---
+++ official/content/home/static/home/js/home_page.js
@@ -0,0 +1,31 @@
+
+$(document)
+  .on('load', function () {
+    $('.masthead .official.header.container').transition(
+      {
+        animation: 'fade',
+        duration: '1s',
+        onComplete: function () {
+          $('.masthead .ui.sub.header').transition({
+            animation: 'fade',
+            duration: '0.6s',
+            onComplete: function () {
+              $('.masthead .ui.primary.button').transition({
+                animation: 'fade',
+                duration: '0.3s',
+                onComplete: function () {
+                  $('.masthead .ui.secondary.button').transition({
+                    animation: 'fade',
+                    duration: '0.3s',
+                  });
+                }
+              }
+              )
+            }
+          }
+          )
+        }
+      }
+    );
+  }
+  )
