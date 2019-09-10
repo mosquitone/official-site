36090e4e89de12a41b04b31f64aec443207b3dcf
Yuusuke Yamashita <yamashita@yyamashita.com>
2019-09-10 10:15:51 +0900

    Allow editor to edit top page link buttons

--- official/content/home/models.py
+++ official/content/home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
-from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import (FieldPanel, MultiFieldPanel,
+                                         PageChooserPanel)
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 
@@ -12,7 +12,34 @@ class HomePage(Page):
         'wagtailimages.Image', null=True, blank=False,
         on_delete=models.SET_NULL, related_name='+'
     )
+    primary_button_link = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    primary_button_text = models.CharField(
+        null=True, blank=True, max_length=25)
+    secondary_button_link = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    secondary_button_text = models.CharField(
+        null=True, blank=True, max_length=25)
+
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
         ImageChooserPanel('background_image'),
+        MultiFieldPanel([
+            FieldPanel('primary_button_text'),
+            PageChooserPanel('primary_button_link'),
+        ], heading="primary button"),
+        MultiFieldPanel([
+            FieldPanel('secondary_button_text'),
+            PageChooserPanel('secondary_button_link'),
+        ], heading="secondary button"),
     ]
