e3c327f017529d70e4c64ab226839d174bb2676a
Yuusuke Yamashita <yamashita@yyamashita.com>
2019-12-03 23:48:44 +0900

    Improve misc style

--- official/content/home/static/home/js/home_page.js
+++ official/content/home/static/home/js/home_page.js
@@ -1,20 +1,20 @@
 
 $(window)
   .on('load', function () {
-    $('.masthead .official.header.container').transition(
+    $('main .official.header.container').transition(
       {
         animation: 'fade',
         duration: '1s',
         onComplete: function () {
-          $('.masthead .ui.sub.header').transition({
+          $('main .ui.sub.header').transition({
             animation: 'fade',
             duration: '0.6s',
             onComplete: function () {
-              $('.masthead .ui.primary.button').transition({
+              $('main .ui.primary.button').transition({
                 animation: 'fade',
                 duration: '0.3s',
                 onComplete: function () {
-                  $('.masthead .ui.secondary.button').transition({
+                  $('main .ui.secondary.button').transition({
                     animation: 'fade',
                     duration: '0.3s',
                   });
