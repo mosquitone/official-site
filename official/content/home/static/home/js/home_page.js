2c34ab17cf72a89705a70c21c231e4328fadccb1
Yuusuke Yamashita <yamashita@yyamashita.com>
2019-12-17 23:12:46 +0900

    Change style of link buttons in homepage
    - Use theme color
    - Refactor transision script more flexibly
    - More top margin
    - Delay link buttons appearance animation

--- official/content/home/static/home/js/home_page.js
+++ official/content/home/static/home/js/home_page.js
@@ -4,24 +4,23 @@ $(window)
     $('main .official.header.container').transition(
       {
         animation: 'fade',
-        duration: '1s',
+        duration: '1.5s',
         onComplete: function () {
           $('main .ui.sub.header').transition({
             animation: 'fade',
             duration: '0.6s',
-            onComplete: function () {
-              $('main .ui.primary.button').transition({
+            onComplete: $('main .official.link.buttons > .button').toArray().reverse().reduce(function (nextButtonTransition, currentButton) {
+              var transitionParameter = {
                 animation: 'fade',
                 duration: '0.3s',
-                onComplete: function () {
-                  $('main .ui.secondary.button').transition({
-                    animation: 'fade',
-                    duration: '0.3s',
-                  });
-                }
               }
-              )
-            }
+              if (nextButtonTransition) {
+                transitionParameter.onComplete = nextButtonTransition;
+              }
+              return function () {
+                $(currentButton).transition(transitionParameter)
+              }
+            }, null)
           }
           )
         }
